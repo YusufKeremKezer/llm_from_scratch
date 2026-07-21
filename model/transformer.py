7d743c4c186b272dbf9e7e1847e532be8e3fe47d
Yusuf Kerem Kezer <yusufkeremkezer@gmail.com>
2026-07-21 10:46:15 +0300

    Add Multihead attention layer - Code repeat itself - Casual and classic attention layers unnecessary - Accumulated everything in multihead attention layer - inputs changed as q,k,v instead of X to support cross attention

--- model/transformer.py
+++ model/transformer.py
@@ -1,68 +1,60 @@
 from torch import nn
 import torch.nn.functional as F
 import torch
-from torch import optim
 import numpy as np
+import math
 
-class AttentionLayer(nn.Module):
+# q.shape = (batch_size,sequence_len,embedding_dim)
+# instead of X inputs renamed as q,k,v because now it supports cross-attention
+# Batch size means the size of sequence length processing in parallel
+# context len means how many words LLM can see
+# Q_proj.shape = Q_proj @ X.transpose() (batch_size,sequence_len,embedding_dim) @ (embedding_dim x embedding_dim).T
+    
+# output Q and K and Vshape = (batch_size,sequence_len,embedding_dim) 
+# K.Transpose shape = (batch_size,embedding_dim,sequence_len)
+
+#scores.shape = (batch_size,sequence_len,sequence_len)
+
+class MHA(nn.module):
     pass
-
-class ClassicAttentionLayer(AttentionLayer): # Can see future to cheat
-    def __init__(self, embedding_dim:int=512):
+class MultiHeadAttention(MHA):
+    def __init__(self, num_heads:int=8, embedding_dim:int=512):
         super().__init__()
-        self.Q = nn.Linear(embedding_dim,embedding_dim)
-        self.K = nn.Linear(embedding_dim,embedding_dim)
-        self.V = nn.Linear(embedding_dim,embedding_dim)
+        self.num_heads = num_heads
+        self.head_dim = embedding_dim/num_heads
 
-    def forward(self,X):
-        Q = self.Q(X)
-        K = self.K(X)
-        V = self.V(X)
-        scores = Q @ K.transpose(-2,-1) # How related each query with keys.
-        attention_weights = F.softmax(scores,dim=-1) # How important affair between them
+        self.q_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.k_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.v_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.out_proj = nn.Linear(embedding_dim,embedding_dim)
+    
+    def forward(self, q, k, v, mask = None):
+        batch ,context_len, _ = q.shape
+        
+        Q = self.q_proj(q).view(batch,  context_len, self.num_heads, self.head_dim).transpose(1,2)
+        K = self.k_proj(k).view(batch,  context_len, self.num_heads, self.head_dim).transpose(1,2)
+        V = self.v_proj(v).view(batch,  context_len, self.num_heads, self.head_dim).transpose(1,2)
 
-        return attention_weights @ V # How important that token
+        scores = torch.matmul(Q, K.transpose(-2,-1)) / math.sqrt(self.head_dim)
 
+        casual_mask = torch.triu(torch.ones(context_len,context_len))
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self,attention_layer,num_heads:int=8,embedding_dim:int=512):
-        super().__init__()
-        self.attention_layers = nn.ModuleList([attention_layer(embedding_dim/num_heads) for _ in range(num_heads)])
+        if mask is not None:
+            scores = scores.masked_fill(casual_mask , mask = float("-inf"))
 
-    def forward(self,X):
-        head_outputs = [layer(X) for layer in self.attention_layers]
-        return torch.cat(head_outputs,dim=-1) # Concatenate all heads
+        logits = torch.softmax(scores, dim = -1)
+        attention_output = torch.matmul(logits,V) 
+        attention_output = attention_output.transpose(1, 2).contiguous().view(batch,context_len,-1)        
+        
+        output = self.out_proj(attention_output) # to communicate between heads
 
+        return output
 
-class CasualAttentionLayer(AttentionLayer):
-    def __init__(self,embedding_dim:int=512):
-        super().__init__()
-        self.Q = nn.Linear(embedding_dim,embedding_dim)
-        self.K = nn.Linear(embedding_dim,embedding_dim)
-        self.V = nn.Linear(embedding_dim,embedding_dim)
-
-    def forward(self,X):
-        # X.shape = (batch_size,sequence_len,embedding_dim)
-        # Batch size means how many sentences we are processing in parallel by predicting next word.
-
-        Q = self.Q(X) # Q.shape = Q @ X.transpose() (batch_size,sequence_len,embedding_dim) @ (embedding_dim x embedding_dim).T
-        K = self.K(X)  
-        V = self.V(X) # output Q and K and Vshape = (batch_size,sequence_len,embedding_dim) 
-        scores = Q @ K.transpose(-2,-1) # How related each query with keys.
-        scores/=np.sqrt(Q.shape[-1])
-        # K.Transpose shape = (batch_size,embedding_dim,sequence_len)
-
-        #scores.shape = (batch_size,sequence_len,sequence_len)
-        mask = torch.triu(torch.full(scores.shape, float('-inf')), diagonal=1) # Masking future tokens with -inf to prevent cheating.
-        scores +=mask
-        attention_weights = F.softmax(scores,dim=-1) # How important affair between them
-
-        return attention_weights @ V.transpose(-2,-1) # How valuable that token is within the context batch_size, seq_len, embedding_dim
 
 class TransformerBlock(nn.Module):
-    def __init__(self,embedding_dim:int=512, attention_layer:nn.Module=CasualAttentionLayer(), num_heads:int=8):
+    def __init__(self,embedding_dim:int=512, attention_layer:nn.Module=MHA, num_heads:int=8):
         super().__init__()
-        self.mha = MultiHeadAttention(attention_layer,num_heads,embedding_dim)
+        self.mha = MHA
         self.ff_dim = embedding_dim*2
         self.feed_forward = nn.Sequential(
             nn.Linear(embedding_dim,self.ff_dim), 
