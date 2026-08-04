c8d8b02f44f43cf6e92ab145f610c37024e4ec50
Yusuf Kerem Kezer <yusufkeremkezer@gmail.com>
2026-08-04 10:02:00 +0300

    Fix: -Apply merge index related problem solved -Clear dictionary to prevent stale parameters in create_word_splits -Clear functions inside BPE

--- tokenization/bpe.py
+++ tokenization/bpe.py
@@ -46,8 +46,8 @@ def create_word_splits(word_freqs):
 
 
 # %%
-merge_pairs = defaultdict(int)
 def count_merge_pairs(word_splits):
+    merge_pairs = defaultdict(int)
     for word, chars in word_splits.items():
         freq = word_freqs[word]
         for i in range(len(chars)-1):
@@ -60,27 +60,41 @@ def count_merge_pairs(word_splits):
 def apply_merge(word_splits, pair):
     char1, char2 = pair
     merged = char1 + char2
+    
     for _, chars in word_splits.items():
-        for i in range(len(chars)-1):
-            if chars[i] == char1 and chars[i+1] == char2:
-                chars[i] = merged
-                chars.pop(i+1)
-                break
+        i = 0
+        new_chars = []
+        while i < len(chars):
+            if i < len(chars)-1 and char1 == chars[i] and char2 == chars[i+1]:
+                new_chars.append(merged)
+                i+=2
+            else:
+                new_chars.append(chars[i])
+                i+=1
+        chars[:] = new_chars
+
     return word_splits
 
 # %% 
 merge_rules = []
+
 def bpe(corpus, num_of_merges):
-    for i in range(num_of_merges):
-        word_freqs = count_word_freqs(corpus)
-        word_splits = create_word_splits(word_freqs)
+    word_freqs = count_word_freqs(corpus)
+    word_splits = create_word_splits(word_freqs)
+    for _ in range(num_of_merges):
         merge_pairs = count_merge_pairs(word_splits)
         best_pair = max(merge_pairs, key=merge_pairs.get)
         merge_rules.append(best_pair)
-        apply_merge(word_splits, best_pair)    
+        word_splits = apply_merge(word_splits, best_pair)    
+    
     return word_splits
     
 word_splits = bpe(corpus, 30)
 # %%
+print(word_splits)
 
+# %%
+print(len(merge_rules))
+# %%
+print(merge_rules)
 # %%
