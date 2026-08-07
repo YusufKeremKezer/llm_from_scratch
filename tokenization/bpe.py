9f5ba417d6b4410d42cfea8a0901031768de0710
Yusuf Kerem Kezer <yusufkeremkezer@gmail.com>
2026-08-07 10:16:37 +0300

    Add bpe inference

--- tokenization/bpe.py
+++ tokenization/bpe.py
@@ -1,100 +1,216 @@
 # %%
 
 from collections import defaultdict
+
 corpus = [
+
     "Artificial intelligence is transforming the world.",
+
     "Cats often nap in sunny windows during the afternoon.",
+
     "Quantum mechanics challenges our classical understanding of reality.",
+
     "A warm cup of coffee can brighten even the coldest morning.",
+
     "Exploring distant planets demands innovative engineering solutions."
+
 ]
+
 
 unique_chars = set()
 
+
 for doc in corpus:
+
     for char in doc:
+
         unique_chars.add(char)
 
 
+
 vocab = list(unique_chars)
+
 vocab.sort()
 
 
+
 eow_token = '</n>'
+
 # Indicating end of a word
+
 vocab.append(eow_token)
+
 
 print(len(vocab))
 
 
+
 # %%
+
 word_freqs = defaultdict(int) # if specified key is not found instead of throwing an error constructs it.
+
 def count_word_freqs(corpus):
+
     for doc in corpus:
+
         words = doc.split(' ')
+
         for word in words:
+
             word_freqs[word] += 1
+
 
     return word_freqs 
 
+
 # %%
+
 word_splits = {}
-def create_word_splits(word_freqs):
+
+def create_word_splits(word_freqs: dict[str, int]):
+
     for word, _ in word_freqs.items():
+
         word_splits[word] = list(word)
     return word_splits
 
 
+
 # %%
-def count_merge_pairs(word_splits):
+
+def count_merge_pairs(word_splits: dict[str, list[str]]):
     merge_pairs = defaultdict(int)
+
     for word, chars in word_splits.items():
+
         freq = word_freqs[word]
+
         for i in range(len(chars)-1):
+
             pair = (chars[i], chars[i+1])
+
             merge_pairs[pair] +=freq
     return merge_pairs
 
+
 # %%
 
-def apply_merge(word_splits, pair):
+
+def apply_merge(word_splits: dict[str, list[str]], pair: tuple[str, str]):
+
     char1, char2 = pair
+
     merged = char1 + char2
     
+
     for _, chars in word_splits.items():
+
         i = 0
+
         new_chars = []
+
         while i < len(chars):
+
             if i < len(chars)-1 and char1 == chars[i] and char2 == chars[i+1]:
+
                 new_chars.append(merged)
+
                 i+=2
+
             else:
+
                 new_chars.append(chars[i])
+
                 i+=1
+
         chars[:] = new_chars
 
     return word_splits
 
+
 # %% 
+
 merge_rules = []
 
-def bpe(corpus, num_of_merges):
+
+def bpe_training(corpus, num_of_merges):
+
     word_freqs = count_word_freqs(corpus)
+
     word_splits = create_word_splits(word_freqs)
+
     for _ in range(num_of_merges):
         merge_pairs = count_merge_pairs(word_splits)
+
         best_pair = max(merge_pairs, key=merge_pairs.get)
+
         merge_rules.append(best_pair)
-        word_splits = apply_merge(word_splits, best_pair)    
+
+        token = apply_merge(word_splits, best_pair)    
     
     return word_splits
     
-word_splits = bpe(corpus, 30)
-# %%
-print(word_splits)
+
+word_splits = bpe_training(corpus, 30)
 
 # %%
+vocab.extend(merge_rules)
+
+token_to_id = {token: idx for idx, token in enumerate(vocab)}
+
+
+
+
+
+def bpe_inference(corpus, merge_rules):
+
+    all_tokens = []
+
+    all_ids = []
+
+
+    for doc in corpus:
+
+        words = doc.split(' ')
+
+        doc_tokens = []
+
+
+        for word in words:
+            chars = list(word)
+
+
+            for pair in merge_rules:
+
+                splits = apply_merge({word: chars}, pair)[word]
+
+
+            doc_tokens.extend(splits)
+
+
+        doc_ids = [token_to_id[token] for token in doc_tokens if token in token_to_id]
+
+
+        all_tokens.append(doc_tokens)
+
+        all_ids.append(doc_ids)
+
+
+    return all_tokens, all_ids
+    
+
+"""
+print(word_splits)
 print(len(merge_rules))
-# %%
 print(merge_rules)
-# %%
+
+"""
+
+
+tokens,ids = bpe_inference(corpus, merge_rules)
+
+
+print("tokens:", tokens)
+
+print("ids:", ids)
+
